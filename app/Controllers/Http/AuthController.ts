7f32c4e123ac7aa228719c734356e0dee5e67c5a
Milan Amatya <amatyamilan@gmail.com>
2022-10-12 09:34:28 +0545

    error handling in auth controller

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -7,9 +7,15 @@ export default class AuthController {
   constructor(private userService: UserService) {}
 
   public async signup({ request }) {
-    const data = await request.validate(CreateUser)
+    try {
+      const data = await request.validate(CreateUser)
 
-    const user = await this.userService.create(data)
-    return user
+      const user = await this.userService.create(data)
+      return user
+    } catch (signupError) {
+      console.error('Error while signing up')
+
+      throw signupError
+    }
   }
 }
