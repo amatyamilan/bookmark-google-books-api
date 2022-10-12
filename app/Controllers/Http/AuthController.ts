29726af7836cbf2fb887a2234370c6dfe680fc2e
Milan Amatya <amatyamilan@gmail.com>
2022-10-12 20:40:27 +0545

    added signin feature

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,6 +1,7 @@
 import CreateUser from 'App/Validators/CreateUserValidator'
 import { inject } from '@adonisjs/fold'
 import UserService from 'App/Services/UserService'
+import Hash from '@ioc:Adonis/Core/Hash'
 
 @inject()
 export default class AuthController {
@@ -11,5 +12,21 @@ export default class AuthController {
 
     const user = await this.userService.create(data)
     return user
+  }
+
+  public async signin({ auth, request, response }) {
+    const { email, password } = request.all()
+
+    if (await auth.attempt(email, password)) {
+      let user = await this.userService.findUserByEmail(email)
+
+      if (!(await Hash.verify(user.password, password))) {
+        return response.unauthorized('Invalid credentials')
+      }
+
+      let token = await auth.use('api').generate(user)
+
+      return token
+    }
   }
 }
